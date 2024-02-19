2bf70f279556cc5f7ccc218772bf2341ae074455
Audrey Smith <audreyas@email.unc.edu>
2024-02-18 20:31:20 -0500

    Progress on EX03

--- exercises/ex03_functional_battleship.py
+++ exercises/ex03_functional_battleship.py
@@ -6,6 +6,8 @@ import random
 BLUE_BOX: str = "\U0001F7E6"
 RED_BOX: str = "\U0001F7E5"
 WHITE_BOX: str = "\U00002B1C"
+
+
 def input_guess(grid_size: int, row_or_column: str) -> int:
     """Prompt and return the user's row or column guess."""
     assert row_or_column == "row" or row_or_column == "column"
@@ -14,13 +16,15 @@ def input_guess(grid_size: int, row_or_column: str) -> int:
         guess = int(input(f"The grid is only {grid_size} by {grid_size}. Try again: "))
     if guess <= grid_size and guess > 0:
         return int(guess)
+    return 0 
+
 
 def print_grid(grid_size: int, row_guess: int, column_guess: int, user_guess: bool) -> None: 
     """Print a grid of boxes to represent the game board."""
-    if user_guess == True:
+    if user_guess is True:
         result_box: str = RED_BOX
-    if user_guess == False:
-        result_box: str = WHITE_BOX
+    if user_guess is False:
+        result_box = WHITE_BOX
     row_counter: int = 1
     while row_counter <= grid_size:
         singular_row: str = ""
@@ -38,39 +42,41 @@ def print_grid(grid_size: int, row_guess: int, column_guess: int, user_guess: bo
                 column_counter += 1
         print(singular_row)
         row_counter += 1
+
+
 def correct_guess(secret_row: int, secret_column: int, row_guess: int, column_guess: int) -> bool:  
     """Given the secret boat location and the user's guess return if user was correct."""
     if secret_row == row_guess and secret_column == column_guess: 
         return True 
     else: 
         return False
+    
+
 def main(grid_size: int, secret_row: int, secret_column: int) -> None: 
-    """Implement the main function""" 
+    """Implement the main function.""" 
     turn_number: int = 1
     while turn_number < 6: 
         print(f"=== Turn {turn_number}/5 ===")
         row_guess = input_guess(grid_size, "row")
         column_guess = input_guess(grid_size, "column")
         corrguess = correct_guess(secret_row, secret_column, row_guess, column_guess)
-        if corrguess == True:
+        if corrguess is True:
             user_guess = True
             print_grid(grid_size, row_guess, column_guess, user_guess)
         else:
             user_guess = False
             print_grid(grid_size, row_guess, column_guess, user_guess)
-        #corrguess = correct_guess(secret_row, secret_column, row_guess, column_guess)
-        if corrguess == True:
+        if corrguess is True:
             print("Hit!")
             print(f"You won in {turn_number}/5 turns!")
             turn_number = 7 
         else: 
             print("Miss!")
             turn_number += 1
-        if turn_number == 6 and corrguess == False: 
+        if turn_number == 6 and corrguess is False: 
             print("X/5 - Better luck next time!") 
+
 
 if __name__ == "__main__":
     grid_size: int = random.randint(3, 5)
     main(grid_size, random.randint(1, grid_size), random.randint(1, grid_size))
-        
-
